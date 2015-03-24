99c641cf4b17e73b41f91799dd7ab621b499f852
Daniel King <dan@llamadigital.net>
2015-03-24 11:02:35 +0000

    remove gitconfig setup prompt

--- install.sh
+++ install.sh
@@ -10,22 +10,3 @@ if [[ -z `which fresh` ]]; then
 else
   fresh
 fi
-
-# gitconfig
-if [ -e $HOME/.gitconfig ]
-then echo ".gitconfig found, doing nothing"
-else
-  read -p "Name " name
-  read -p "Email " email
-  read -p "Github username " github
-
-  # escape strings for sed
-  name=$(printf "%s\n" "$name" | sed 's/[\&/]/\\&/g')
-  email=$(printf "%s\n" "$email" | sed 's/[\&/]/\\&/g')
-  github=$(printf "%s\n" "$github" | sed 's/[\&/]/\\&/g')
-  home=$(printf "%s\n" "$HOME" | sed 's/[\&/]/\\&/g')
-
-  # replace NAME, EMAIL, GITHUB, HOME
-  sed "s/NAME/$name/g" $DIR/gitconfig | sed "s/EMAIL/$email/g" | sed "s/GITHUB/$github/g" | sed "s/HOME/$home/g" > $HOME/.gitconfig
-  echo ".gitconfig installed"
-fi
